822d743a73b3856f5df9ae7af88a2330920f4645
Praveen Tamminaina <tamminainapraveen@gmail.com>
2026-03-05 17:54:59 +0530

    corrections in boundary code

--- 2d_arrays/Boundary.cpp
+++ 2d_arrays/Boundary.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 int main()
 {
-
   int n, m;
   cin >> n >> m;
 
@@ -18,7 +17,7 @@ int main()
     }
   }
 
-  cout << "Normal form: " << endl;
+  cout << "Normal form:\n";
 
   for (int i = 0; i < n; i++)
   {
@@ -29,42 +28,30 @@ int main()
     cout << endl;
   }
 
-  cout << "Boundary form: " << endl;
+  cout << "Boundary form:\n";
 
   if (n == 1)
   {
     for (int i = 0; i < m; i++)
-    {
       cout << arr[0][i] << " ";
-    }
   }
   else if (m == 1)
   {
     for (int i = 0; i < n; i++)
-    {
-      cout << arr[0][i] << " ";
-    }
+      cout << arr[i][0] << " ";
   }
   else
   {
     for (int i = 0; i < m; i++)
-    {
       cout << arr[0][i] << " ";
-    }
 
     for (int i = 1; i < n; i++)
-    {
-      cout << arr[i][n - 1] << " ";
-    }
+      cout << arr[i][m - 1] << " ";
 
     for (int i = m - 2; i >= 0; i--)
-    {
       cout << arr[n - 1][i] << " ";
-    }
 
     for (int i = n - 2; i >= 1; i--)
-    {
       cout << arr[i][0] << " ";
-    }
   }
 }
