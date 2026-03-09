77a448478b19291c2cd2a5095120d69e411af3ff
Praveen Tamminaina <tamminainapraveen@gmail.com>
2026-03-09 23:31:25 +0530

    string compression

--- practice1/string_compression.cpp
+++ practice1/string_compression.cpp
@@ -1,32 +1,31 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void compress()
-{
-  string str;
-  cin >> str;
-
-  for (int i = 0; i < str.size();)
-  {
-    int j = i;
-    char temp = str[i];
-    while (j < str.size() && str[i] == str[j])
-    {
-      j++;
+void compress() {
+    string str;
+    cin >> str;
+    
+    int i=0;
+    while(i < str.size()) {
+        int j = i;
+        char temp = str[i];
+        while(j < str.size() && str[i] == str[j]) {
+            j++;
+        }
+        cout << str[i];
+        if(j - i > 1) {
+            cout << j - i;
+        }
+        i = j;
     }
-    cout << str[i] << j - i;
-    i = j;
-  }
-  cout << endl;
+    cout << endl;
 }
 
-int main()
-{
-  int n;
-  cin >> n;
-
-  for (int i = 0; i < n; i++)
-  {
-    compress();
-  }
+int main() {
+    int n;
+    cin >> n;
+    
+    for(int i=0;i<n;i++) {
+        compress();
+    }
 }
