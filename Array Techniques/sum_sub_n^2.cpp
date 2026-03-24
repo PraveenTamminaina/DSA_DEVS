37d148cee60606fd63ea37b2d5631b76b7e4bba3
Praveen Tamminaina <tamminainapraveen@gmail.com>
2026-03-24 19:16:30 +0530

    sum of sub arrays in O(n^2) time complexity making it faster using \n

--- Array Techniques/sum_sub_n^2.cpp
+++ Array Techniques/sum_sub_n^2.cpp
@@ -1,20 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    
-    int arr[n];
-    for(int i=0;i<n;i++) {
-        cin >> arr[i];
+int main()
+{
+  int n;
+  cin >> n;
+
+  int arr[n];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+
+  for (int l = 0; l < n; l++)
+  {
+    int sum = 0;
+    for (int r = l; r < n; r++)
+    {
+      sum += arr[r];
+      cout << sum << "\n";
     }
-    
-    for(int l=0;l<n;l++) {
-        int sum = 0;
-        for(int r=l;r<n;r++) {
-            sum += arr[r];
-            cout << sum << endl;
-        }
-    }
+  }
 }
